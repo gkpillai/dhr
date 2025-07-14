b6193424a407b891026f39c506a5aad05d516ca4
gkpillai <dhr@atmantan.com>
2025-07-14 21:00:10 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,29 @@ function addScript(src) {
   document.body.appendChild(script);
 }
 
+const empNo = sessionStorage.getItem("empNo");
 
+if (!empNo) {
+  // Not logged in, redirect to login
+  navigateToPage("login");
+} else {
+  google.script.run.withSuccessHandler(function (data) {
+    const nameTitle = toTitleCase(data.name || "Unknown");
+    const designationTitle = toTitleCase(data.designation || "Unknown");
+
+    const nameEl = document.querySelector(".sidebar-userpic-name");
+    const desigEl = document.querySelector(".profile-usertitle-job");
+
+    if (nameEl) nameEl.innerText = nameTitle;
+    if (desigEl) desigEl.innerText = designationTitle;
+  }).getEmployeeDetails(empNo);
+}
+
+function toTitleCase(str) {
+  return str.toLowerCase().replace(/\b\w/g, function(txt) {
+    return txt.toUpperCase();
+  });
+}
 
 function logout() {
   sessionStorage.clear();
